014a073df3094493bd49b8f6ebb9f3864bf47c19
Jarrod Chesney <jarrod.chesney@synerty.com>
2018-07-02 18:51:01 +1000

    compile with TS 2.7.2, ORMOB-644

--- ng2-balloon-msg-ns/ng2-balloon-msg-ns.module.ts
+++ ng2-balloon-msg-ns/ng2-balloon-msg-ns.module.ts
@@ -1,21 +1,22 @@
-import {NgModule} from "@angular/core";
-import {CommonModule} from "@angular/common";
-import {Ng2BalloonMsgNsQueueComponent} from "./queue/ng2-balloon-msg-ns-queue.component";
-import {Ng2BalloonMsgNsItemComponent} from "./item/ng2-balloon-msg-ns-item.component";
+import {NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {Ng2BalloonMsgNsQueueComponent} from './queue/ng2-balloon-msg-ns-queue.component';
+import {Ng2BalloonMsgNsItemComponent} from './item/ng2-balloon-msg-ns-item.component';
 
 @NgModule({
-    imports: [
-        CommonModule,
-    ],
-    exports: [
-        Ng2BalloonMsgNsQueueComponent,
-        Ng2BalloonMsgNsItemComponent
-    ],
-    declarations: [
-        Ng2BalloonMsgNsQueueComponent,
-        Ng2BalloonMsgNsItemComponent
-    ],
-    providers: []
+  imports: [
+    CommonModule,
+  ],
+  exports: [
+    Ng2BalloonMsgNsQueueComponent,
+    Ng2BalloonMsgNsItemComponent
+  ],
+  declarations: [
+    Ng2BalloonMsgNsQueueComponent,
+    Ng2BalloonMsgNsItemComponent
+  ],
+  providers: [],
+  schemas: [NO_ERRORS_SCHEMA]
 })
 export class Ng2BalloonMsgNsModule {
 
